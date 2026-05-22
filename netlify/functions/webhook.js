bde6642868f500dfdfbb9aa2daf8ecd6c2cab70c
Barlog-1PV <barlog.1pv@gmail.com>
2026-05-23 00:46:03 +0200

    Added more mobile support Added some security on the webhook link

--- netlify/functions/webhook.js
+++ netlify/functions/webhook.js
@@ -4,7 +4,7 @@ exports.handler = async (event, context) => {
     }
 
     try {
-        const WEBHOOK_URL = 'https://discord.com/api/webhooks/1496570610439159949/4KK5f5NxRtDp2k8UdzInqapbJvTvtGPoxakMvItlQ82Ggm_pCS5qaPnjpYNF6D3Q7yM6';
+        const WEBHOOK_URL = process.env.DISCORD_WEBHOOK_URL;
 
         const bodyBuffer = event.isBase64Encoded ? Buffer.from(event.body, 'base64') : event.body;
 
